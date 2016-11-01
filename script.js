afc25d045cb7b368e61511e8ac14dafa94bbeb8c
heidipowers <heidipowers@gmail.com>
2016-10-31 23:51:20 -0400

    Changed scroll speed

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
             if (target.length) {
                 $('html, body').animate({
                     scrollTop: target.offset().top
-                }, 1000);
+                }, 800);
                 return false;
             }
         }
