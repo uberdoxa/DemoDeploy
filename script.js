fca0d7bb05e801cd3c44320f7439edfd1c67dd7e
heidipowers <heidipowers@gmail.com>
2016-10-31 23:41:37 -0400

    Ahhh man, like everything

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    console.log("sledgehammer...");
+
+    var $work = $('.work');
+    var $text = $('.text');
+    $work.hover(
+        function() {
+            console.log($(this))
+            $(this).find($text).toggle();
+        }
+    );
+
+
+    //Thanks CSS tricks!
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    //end file
+})
